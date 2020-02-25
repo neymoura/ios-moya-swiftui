55b66392cb0e9490ad504f13dfeabfd4c62b6e10
Ney Moura <neymourac@gmail.com>
2020-02-25 16:44:21 -0300

    Initial Commit

---
+++ moyasample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moyasample
+//
+//  Created by Ney Moura on 25/02/20.
+//  Copyright © 2020 Ney Moura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
